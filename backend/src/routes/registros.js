4ae887f2b2ab4ff02d7b3a1651709f664c56acc4
felixfv25 <felixfv.escorpio25@gmail.com>
2026-09-07 17:53:00 -0500

    Update registros.js

--- backend/src/routes/registros.js
+++ backend/src/routes/registros.js
@@ -32,7 +32,7 @@ function horaActualPeru() {
 
 // Horario de atencion: 8:30am a 3:30pm, hora de Peru (America/Lima).
 const HORA_APERTURA = "08:00";
-const HORA_CIERRE = "15:30";
+const HORA_CIERRE = "20:30";
 
 function dentroDeHorarioAtencion() {
   const horaLima = new Intl.DateTimeFormat("es-PE", {
